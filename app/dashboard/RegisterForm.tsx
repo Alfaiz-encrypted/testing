cbe395cfd37fd8fcdf910c4faf09dea88011e36d
Alfaiz-encrypted <155072532+Alfaiz-encrypted@users.noreply.github.com>
2023-12-28 17:34:08 +0530

    Update RegisterForm.tsx

--- app/dashboard/RegisterForm.tsx
+++ app/dashboard/RegisterForm.tsx
@@ -1,92 +1,56 @@
-'use client'
+import Link from 'next/link';
 
-import React, { useState } from 'react';
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <ul className="nav-list">
+                <li className="nav-item">
+                    <Link href="/blog/blog1">
+                        bloag1
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link href="/blog/blog2">
+                        bloag2
+                    </Link>
+                </li>
+                {/* <li className="nav-item">
+                    <Link href="/contact">
+                        Contact
+                    </Link>
+                </li> */}
+                {/* Add more links as needed */}
+            </ul>
 
-const RegisterForm: React.FC = () => {
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-  });
+            {/* Add additional styling or components here */}
+            <style jsx>{`
+        .navbar {
+          background-color: rgb(33, 10, 48);
+          padding: 10px;
+        }
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+        .nav-list {
+          list-style: none;
+          margin: 0;
+          padding: 0;
+          display: flex;
+          color:white;
+        }
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+        .nav-item {
+          margin-right: 15px;
+          color:white
+        }
 
-    // Log the form data for debugging
-    console.log('Form data:', formData);
-
-    try {
-      // Send registration data to the API
-      const response = await fetch('/api/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-
-      if (response.ok) {
-        // Registration successful, handle as needed
-        console.log('Registration successful');
-      } else {
-        // Registration failed, handle error
-        console.error('Registration failed');
-      }
-    } catch (error) {
-      console.error('Registration error:', error);
-    }
-  };
-
-  return (
-    <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-8" style={{ backgroundColor: '#ADD8E6', margin: 'auto', padding: '20px', borderRadius: '10px'}}>
-      <div className="mb-4">
-        <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">
-          Username:
-        </label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
-          Email:
-        </label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
-          Password:
-        </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        />
-      </div>
-      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-        Register
-      </button>
-    </form>
-  );
+        .nav-link {
+          text-decoration: none;
+          color: white;
+          font-weight: bold;
+          font-size: 16px;
+        }
+      `}</style>
+        </nav>
+    );
 };
 
-export default (RegisterForm);
+export default Navbar;
